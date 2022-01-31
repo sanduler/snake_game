100c56d64288f94cc873e46fd56a3cce9dd941d3
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-31 09:02:33 -0800

    created the main screen and changed the background color

--- main.py
+++ main.py
@@ -2,4 +2,18 @@
 # Date: January 31st, 2022
 # Description: The main goal of the game is to create a fully functional snake game.
 
+from turtle import Turtle, Screen
 
+# create the screen for the game.
+import black as black
+
+screen = Screen()
+# set up the size for the screen.
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+# screen doesn't close automatically
+screen.exitonclick()
